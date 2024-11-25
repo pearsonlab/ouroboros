a53368f1861062ff68b242b46250c0c9f283cf28
mdmarti <miles.martinez@duke.edu>
2024-11-25 14:07:58 -0500

    finalizing train and model changes from test case

--- model.py
+++ model.py
@@ -368,6 +368,22 @@ class structured_ouroboros(ouroboros):
         return (zdot,caches)
         
 
+class NonNegClipper(object):
+
+    def __init__(self,min=0):
+        self.min=0
+
+    def __call__(self,module):
+
+        if hasattr(module,'weight'):
+            w = module.weight.data
+            w.clamp_(min=self.min)
+        if hasattr(module,'bias'):
+            b = module.bias.data
+            b.clamp_(min=self.min)
+
+
+plt.rcParams.update({'text.usetex':True})
 class constrained_ouroboros(ouroboros):
 
 
@@ -388,7 +404,9 @@ class constrained_ouroboros(ouroboros):
                 good_init=True,
                 tau = 1000,
                 omega=4000,
-                noInput = False):
+                noInput = False,
+                omega_scale = 10000,
+                smooth_penalty=lambda x: torch.var(x,dim=1)):
         
         super(constrained_ouroboros,self).__init__(d_data,\
                  d_control,\
@@ -402,18 +420,35 @@ class constrained_ouroboros(ouroboros):
                  expand_factor_data,\
                  expand_factor_control,\
                  device)
-        
-        self.b_net = nn.Linear(in_features=d_control,out_features=d_data * poly_dim**2,device=device)
+
+        self.clipper = NonNegClipper(min=0)
+        self.smooth_penalty = smooth_penalty
+        #self.b_net = nn.Linear(in_features=d_control,out_features=d_data * (poly_dim+1)**2,device=device)
+        #self.b_net.weight = nn.Parameter(torch.zeros(self.b_net.weight.shape,device=device),requires_grad=False)
         #print(self.b_net.weight)
-        torch.nn.init.uniform_(self.b_net.weight,a=-1/tau,b=1/tau)
+        #torch.nn.init.uniform_(self.b_net.weight,a=-1/tau**2,b=1/tau**2)
         #print(self.b_net.weight)
-        torch.nn.init.uniform_(self.b_net.bias,a=-1/tau,b=1/tau)
+        #torch.nn.init.uniform_(self.b_net.bias,a=-1/tau**2,b=1/tau**2)
         # d data above: since we only parameterize  d2y/dt2
-        self.omega_net = nn.Linear(in_features=d_control,out_features=d_data,device=device)
-        torch.nn.init.uniform_(self.omega_net.weight,a=-1/tau,b=1/tau)
-        torch.nn.init.uniform_(self.omega_net.bias,a=-1/tau,b=1/tau)
+        self.omega_net = nn.Linear(in_features=d_control,out_features=d_data,device=device) #unconstrained
+        self.gamma_net = nn.Linear(in_features=d_control,out_features=d_data,device=device) # output unconstrained, but weights nonneg
+        self.b_net = nn.Linear(in_features=d_control,out_features=d_data,device=device) # weights non-neg
+        self.b = nn.Parameter(torch.zeros(1),requires_grad=True).to(device)
+        #torch.nn.init.uniform_(self.b,a=0,b=2/tau)
+        self.d_net = nn.Linear(in_features=d_control,out_features=d_data,device=device)
+        torch.nn.init.uniform_(self.omega_net.weight,a=-omega_scale/tau,b=omega_scale/tau)
+        torch.nn.init.uniform_(self.omega_net.bias,a=-omega_scale/tau,b=omega_scale/tau)
+        torch.nn.init.uniform_(self.gamma_net.weight,a=0,b=2/tau)
+        torch.nn.init.uniform_(self.gamma_net.bias,a=0,b=2/tau)
+        torch.nn.init.uniform_(self.b_net.weight,a=-1/tau,b=1/tau)
+        torch.nn.init.uniform_(self.b_net.bias,a=-1/tau,b=1/tau)
+        torch.nn.init.uniform_(self.d_net.weight,a=-1/tau**2,b=1/tau**2)
+        torch.nn.init.uniform_(self.d_net.bias,a=-1/tau**2,b=1/tau**2)
+
         self.tau = tau
         self.omega=omega
+        if noInput:
+            print("running with no input")
         if good_init:
             b_params = torch.zeros(self.b_net.weight.shape,device=device)
 
@@ -428,9 +463,253 @@ class constrained_ouroboros(ouroboros):
 
             
         self.noInput = noInput
-        self.poly_dim = poly_dim
+        self.poly_dim = poly_dim+1
         self.d_data = d_data 
-        self.powers = torch.arange(0,poly_dim,device=device)
+        self.powers = torch.arange(0,poly_dim+1,device=device)
+
+    def _clip_weights(self):
+
+        self.gamma_net.apply(self.clipper)
+        #self.b.apply(self.clipper)
+        #self.b = self.b.clamp(min=0)
+        #self.b_net.apply(self.clipper)
+        
+
+    def forward(self,x,dt,idx):
+        """
+        predicts second derivative at time t.
+        all other predictions should be done in the train look (train_utils.py)
+        """
+
+        B,L,d = x.shape
+        # x: x_0, x_dt, x_2dt,...
+
+        xdot= deriv_approx_dy(x) # this gives: dxdt (currently unitless)
+        
+        # dx: dx_4dt,dx_5dt,dx_6dt,..., dx_(L-4)dt
+        z = torch.cat([x[:,4:-4,:],xdot],dim=-1)
+        L = z.shape[1]
+        x_in = torch.cat([z, torch.flip(z,[1])],dim=-1)
+        
+        states = self.controlMamba(self.control_proj(x_in))
+        states = torch.flip(states,[1])
+        
+        b = nn.ReLU()(self.b_net(states))/model.tau #.view(B,L,d,self.poly_dim,self.poly_dim) # B x L x 2d x P x P
+        d = nn.ReLU()(self.d_net(states))
+        if self.noInput:
+            d = torch.zeros(d.shape,device='cuda')
+        omega = self.omega_net(states)
+        gamma = self.gamma_net(states)/model.tau
+        
+        #print(omega[0,:40,...])
+        #print(omega[0,:40,0],2*np.pi*self.omega)
+        #b[:,:,:,1,0] = -(omega**2)
+        #b[:,:,:,0,1] = gamma
+        """
+        if self.noInput:
+            b[:,:,:,0,0] = 0
+        if idx %100 == 0:
+            ax = plt.gca()
+            ax.plot(omega[0,:40,0].detach().cpu().numpy()*model.tau,label=r'$\omega(t)$')
+            ax.plot(gamma[0,:40,0].detach().cpu().numpy()*model.tau,label=r'$\gamma(t)$')
+            ax.set_ylabel("omega,gamma")
+            ax.legend()
+            ax2=ax.twinx()
+            ax2.plot(b[0,:40,0,1,1].detach().cpu().numpy()*model.tau,label=r'$g(y,\dot{y},t)$',color='tab:green')
+            ax2.set_ylabel("g()")
+            ax2.legend()
+            plt.show()
+            plt.close()
+            ax = plt.gca()
+            ax.plot(torch.diff(omega,dim=1)[0,:40,0].detach().cpu().numpy()*model.tau,label=r'$\omega(t)$ diff')
+            ax.legend()
+            ax2=ax.twinx()
+            ax2.plot(torch.diff(b,dim=1)[0,:40,0,1,1].detach().cpu().numpy()*model.tau,label=r'$g(y,\dot{y},t)$ diff',color='tab:green')
+            ax2.set_ylabel("g()")
+            ax2.legend()
+            plt.show()
+            plt.close()
+            #assert False
+        """
+        #b*= self.tau**2
+        z[:,:,-1] /= dt
+        z1 = z[:,:,:1]
+        power_mat_z1 = z[:,:,:1]**2 #z[:,:,:1,None].expand(-1,-1,-1,self.poly_dim) # B x L x d -> B x L x 2d x P
+        z2 = z[:,:,1:]#,None].expand(-1,-1,-1,self.poly_dim) # B x L x d -> B x L x 2d x P
+        #power_mat_z1 = power_mat_z1.pow(self.powers)
+        #power_mat_z2 = power_mat_z2.pow(self.powers)
+        #pow1 = torch.einsum('bldjk,bldj->bldk',b,power_mat_z1)
+        yhat = -(omega**2)*z1 + gamma * z2 - b*power_mat_z1 * z2 - d #torch.einsum('bldk,bldk->bld',pow1,power_mat_z2)
+        #print(torch.amax(power_mat_z1))
+        #print(torch.amax(power_mat_z2))
+
+        #db = torch.diff(b,dim=1)
+        smooth_penalty = self.smooth_penalty(torch.diff(omega,dim=1)[:,1:,:]).mean() + self.smooth_penalty(torch.diff(gamma,dim=1)[:,1:,:]).mean() + self.smooth_penalty(torch.diff(b,dim=1)[:,1:,:]).mean()#(torch.diff(omega,dim=1)[:,1:,:]**2).mean() + (torch.diff(gamma,dim=1)[:,1:,:]**2).mean() +\
+                        #(torch.diff(b,dim=1)[:,1:,:]**2).mean()
+        #                     (torch.diff(d,dim=1)**2).sum(dim=(-1,-2)).mean()        #  +       
+        #(db[:,:,:,2:self.poly_dim,:]**2).sum(dim=(-1,-2,-3)).mean() + (db[:,:,:,:,2:self.poly_dim]**2).sum(dim=(-1,-2,-3)).mean() +\
+                        #(db[:,:,:,1,1]**2).sum(dim=(-1,-2)).mean()
+        size_penalty =  (d).abs().mean()#b.pow(2).sum(dim=(-1,-2)).mean() #*self.tau**2).abs().pow(2)#.sum(dim=(-1,-2)).mean()
+        #b[:,:,:,2:self.poly_dim,:].abs().pow(2).sum(dim=(-1,-2,-3)).mean() + b[:,:,:,:,2:self.poly_dim].abs().pow(2).sum(dim=(-1,-2,-3)).mean() +\
+        #b[:,:,:,1,1].abs().pow(2).sum(dim=(-1,-2)).mean()
+        #print(size_penalty)
+        return yhat,states,size_penalty + smooth_penalty/3
+
+    def get_funcs(self,x,dt):
+
+        B,_,d = x.shape
+        # x: x_0, x_dt, x_2dt,...
+    
+        xdot= deriv_approx_dy(x)
+        # dx: dx_4dt,dx_5dt,dx_6dt,..., dx_(l-4)dt
+    
+        z = torch.cat([x[:,4:-4,:],xdot],dim=-1)
+        L = z.shape[1]
+        x_in = torch.cat([z, torch.flip(z,[1])],dim=-1)
+        states = self.controlMamba(self.control_proj(x_in))
+        states = torch.flip(states,[1])
+        
+        #nn.ReLU()(self.b_net(states))#.view(B,L,d,self.poly_dim,self.poly_dim) # B x L x 2d x P x P
+        d = nn.ReLU()(self.d_net(states))
+        if self.noInput:
+            d = torch.zeros(d.shape,device='cuda')
+        omega = self.omega_net(states)
+        gamma = self.gamma_net(states)
+        
+        omega *= self.tau
+        gamma *= self.tau
+        d *= self.tau**2
+    
+        z /= dt
+        z1 = z[:,:,:1]
+        z2 = z[:,:,1:]
+        power_mat_z1 = z[:,:,:1].pow(2) #expand(-1,-1,-1,model.poly_dim) # B x 2d -> B x 2d x P
+
+        b = nn.ReLU()(self.b_net(states)) #self.b * torch.ones(power_mat_z1.shape,device='cuda')
+        b *= self.tau
+        b_out =b * power_mat_z1 * z2
+        
+    
+        
+        return omega,gamma,b,b_out,d,states
+        
+
+    def integrate(self,x,dt):
+
+        B,_,d = x.shape
+        # x: x_0, x_dt, x_2dt,...
+
+        xdot= deriv_approx_dy(x)
+        # dx: dx_4dt,dx_5dt,dx_6dt,..., dx_(l-4)dt
+        #L = xdot.shape[1]
+        z = torch.cat([x[:,4:-4,:],xdot],dim=-1)
+        L = z.shape[1]
+        x_in = torch.cat([z, torch.flip(z,[1])],dim=-1)
+        z0 = z[:,0,:]
+        z0[:,-1] /= dt
+        
+        states = self.controlMamba(self.control_proj(x_in))
+        states = torch.flip(states,[1])
+
+        b = nn.ReLU()(self.b_net(states))#.view(B,L,d,self.poly_dim,self.poly_dim) # B x L x 2d x P x P
+        d = nn.ReLU()(self.d_net(states))
+        if self.noInput:
+            d = torch.zeros(d.shape,device='cuda')
+        omega = self.omega_net(states)
+        gamma = nn.ReLU()(self.gamma_net(states))
+        #if self.noInput:
+        #    b[:,:,:,0,0] = 0
+        
+        #b[:,:,:,1,0] = -(omega**2) # this means we multiply here by tau**2
+        b *= self.tau #/dt**2
+        b = b.detach().cpu().numpy()
+        pows = self.powers.detach().cpu().numpy()
+        def dz(t,z):
+
+            # t: time, should have a timestep of roughly 1. treat as ZOH
+            # z: B x 2d
+            b_ind = int(t)
+            b_step = self.b * self.tau**2 #b[0,b_ind,:,:,:]
+            omega_step = omega[:,b_ind,:]
+            gamma_step = gamma[:,b_ind,:]
+            d_step = d[:,b_ind,:]
+            
+            #power_mat_z1 = np.tile(z[:1,None],(1,self.poly_dim)) # B x 2d -> B x 2d x P
+            #power_mat_z2 = np.tile(z[1:,None],(1,self.poly_dim)) # B x 2d -> B x 2d x P
+            #power_mat_z1 = np.power(power_mat_z1,pows)
+            #power_mat_z2 = np.power(power_mat_z2,pows)
+            z1 = z[:1]
+            power_mat_z1 = z[:1]**2 #z[:,:,:1,None].expand(-1,-1,-1,self.poly_dim) # B x L x d -> B x L x 2d x P
+            z2 = z[1:]
+            #pow1 = np.einsum('djk,dj->dk',b_step,power_mat_z1)
+            dz2 = -(omega_step**2)*z1 + gamma_step * z2 - b_step*power_mat_z1 * z2 - d_step #np.einsum('dk,dk->d',pow1,power_mat_z2)
+            
+            dz1 = z[1]
+            
+            return np.hstack([dz1,dz2])
+        t_steps = np.arange(0,L*dt + dt/2,dt)[:L]
+        obj = solve_ivp(dz,(0,L*dt),z0.squeeze().detach().cpu().numpy(),t_eval=t_steps,method='Radau')
+
+        return obj.y
+    
+class kernel_ouroboros(ouroboros):
+
+
+    def __init__(self,d_data,
+                 d_control,
+                 d_out=1,
+                 n_layers_data=2,
+                 n_layers_control=2,
+                 d_state_data=16,
+                 d_state_control=16,
+                 d_conv_data=4,
+                 d_conv_control=4,
+                 expand_factor_data=1,
+                 expand_factor_control=1,
+                 device='cuda',
+                 n_kernels=2,
+                 tau = 1000,
+                 omega=4000,
+                 noInput = False):
+        
+        super(constrained_ouroboros,self).__init__(d_data,\
+                 d_control,\
+                 d_out,\
+                 n_layers_data,\
+                 n_layers_control,\
+                 d_state_data,\
+                 d_state_control,\
+                 d_conv_data,\
+                 d_conv_control,\
+                 expand_factor_data,\
+                 expand_factor_control,\
+                 device)
+        
+        self.weights = nn.Linear(in_features=d_control,out_features=n_kernels,device=device)
+        self.centers = nn.Linear(in_features=d_control,out_features=2*n_kernels,device=device)
+        self.variances = nn.Linear(in_features=d_control,out_features=2*n_kernels,device=device)
+        #print(self.b_net.weight)
+        torch.nn.init.uniform_(self.b_net.weight,a=-1/tau,b=1/tau)
+        #print(self.b_net.weight)
+        torch.nn.init.uniform_(self.b_net.bias,a=-1/tau,b=1/tau)
+        # d data above: since we only parameterize  d2y/dt2
+        self.omega_net = nn.Linear(in_features=d_control,out_features=d_data,device=device)
+        self.gamma_net = nn.Linear(in_features=d_control,out_features=d_data,device=device)
+        
+        self.tau = tau
+        self.omega=omega
+            
+        self.noInput = noInput
+        self.n_kernels = n_kernels
+        self.d_data = d_data 
+        #self.powers = torch.arange(0,poly_dim,device=device)
+
+    def _apply_kernels(self,x,centers,variances):
+
+        ### assumes x is already stacked [y,ydot]
+
+        pass
+
 
 
     def forward(self,x,dt):
@@ -566,39 +845,7 @@ class constrained_ouroboros(ouroboros):
         obj = solve_ivp(dz,(0,L*dt),z0.squeeze().detach().cpu().numpy(),t_eval=t_steps,method='Radau')
 
         return obj.y
-    
-class kernel_ouroboros(ouroboros):
 
-
-    def __init__(self,d_data,
-                 d_control,
-                 d_out=1,
-                 n_layers_data=2,
-                 n_layers_control=2,
-                 d_state_data=16,
-                 d_state_control=16,
-                 d_conv_data=4,
-                 d_conv_control=4,
-                 expand_factor_data=1,
-                 expand_factor_control=1,
-                 device='cuda',
-                 n_kernels=2,
-                 tau = 1000,
-                 omega=4000,
-                 noInput = False):
-        
-        super(constrained_ouroboros,self).__init__(d_data,\
-                 d_control,\
-                 d_out,\
-                 n_layers_data,\
-                 n_layers_control,\
-                 d_state_data,\
-                 d_state_control,\
-                 d_conv_data,\
-                 d_conv_control,\
-                 expand_factor_data,\
-                 expand_factor_control,\
-                 device)
 
 
 
--- train/train.py
+++ train/train.py
@@ -2,14 +2,15 @@ from torch.utils.tensorboard import SummaryWriter
 import torch
 import numpy as np
 from tqdm import tqdm
+from utils import deriv_approx_d2y,deriv_approx_dy
+import matplotlib.pyplot as plt
 
-
-def train(model,optimizer,loss_fn,loaders,filter=None,scheduler=None,nEpochs=100,val_freq=25,mask_prob_aud = 0.1,init_len = 200,runDir='.',dt=1/44100):
+def train(model,optimizer,loss_fn,loaders,filter=None,scheduler=None,nEpochs=100,val_freq=25,mask_prob_aud = 0.1,init_len = 200,runDir='.',dt=1/44100,vis_freq=100):
 
     writer = SummaryWriter(log_dir=runDir)
 
     train_losses,val_losses=[],[]
-    dt_scale = torch.FloatTensor([dt**2])[None,None,:].to('cuda')
+    #dt_scale = torch.FloatTensor([dt**2])[None,None,:].to('cuda')
     
     for epoch in tqdm(range(nEpochs),desc='training model'):
 
@@ -19,21 +20,98 @@ def train(model,optimizer,loss_fn,loaders,filter=None,scheduler=None,nEpochs=100
 
             optimizer.zero_grad()
             x,y = batch # each is bsz x seq len x n neurons + 1
-            bsz,L,n = x.shape
+            bsz,_,n = x.shape
 
-            x = x.to('cuda')
-            y = y.to('cuda')
+            x = x.to('cuda').to(torch.float64)
+            y = y.to('cuda').to(torch.float64)
+            #tx = torch.arange(dt/2,0.3 + dt/2,dt)[:L]
+            #sins = torch.sin(2*np.pi*2000*tx).to('cuda')
+            #coss = torch.cos(2*np.pi*2000*tx).to('cuda')
+            #length = int(round(0.3/dt))
+            #print(length)
+            #print(L)
             
-            dy = y - x
-            dy2 = torch.diff(dy,dim=1)
+            dy = deriv_approx_dy(x)
+            # dy_2dt, dy_3dt, ...., dy_(L-2)dt
+            dy2 = deriv_approx_d2y(x)
+            # d2y_4dt, d2y_5dt, ..., d2y_(L-4)dt            
+            
+            yhat,state_pred,penalty = model(x,dt,idx) #state: B x L x SD
+            #print((dt*model.tau)**2)
+            yhat = yhat * (model.tau*dt)**2
+            #yhat = torch.cat([yhat[:,:-1,:] + dy[:,:-1,:],yhat[:,1:,:]],dim=-1) # one-step prediction too
+            y = dy2#torch.cat([dy[:,1:,:],dy2[:,1:,:]],dim=-1)
+            L = y.shape[1]
 
+            #sins = torch.sin(2*np.pi*2000*tx).to('cuda')
+            if (idx % vis_freq) == 0:
+                with torch.no_grad():
+                    omega,gamma,b,b_out,d,_ = model.get_funcs(x[:1,:,:],dt)
+                    omega,gamma,b,b_out,d = omega.detach().cpu().numpy(),gamma.detach().cpu().numpy(),\
+                                        b.detach().cpu().numpy(),\
+                                        b_out.detach().cpu().numpy(),\
+                                        d.detach().cpu().numpy()
+                    torch.cuda.empty_cache()
+                on = np.random.choice(L - 45)
+                t = np.arange(on,on+40,1)*dt
+                ax = plt.gca()
+                ax.plot(t,omega[0,on:on+40,0]/ (2*np.pi))
+                ax.set_title('omega')
+                plt.show()
+                plt.close()
+                ax = plt.gca()
+                ax.plot(t,gamma[0,on:on+40,0] / (2*np.pi))
+                ax.set_title('gamma')
+                plt.show()
+                plt.close()
+                ax = plt.gca()
+                ax.plot(t,b[0,on:on+40,0]/ (2*np.pi))
+                ax.set_title('b')
+                plt.show()
+                plt.close()
+                ax = plt.gca()
+                ax.plot(t,d[0,on:on+40,0] / (2*np.pi))
+                ax.set_title('d')
+                plt.show()
+                plt.close()
+                #ax = plt.gca()
+                #ax.plot(yhat[0,:40,1].detach().cpu().numpy(),label='prediction')
+                #ax.plot(y[0,:40,1].detach().cpu().numpy(),label='data')
+                #ax.legend()
+                #ax.set_title("dy2")
+                #plt.show()
+                #plt.close() 
+                
+                ax = plt.gca()
+                ax.plot(t,yhat[0,on:on+40,0].detach().cpu().numpy(),label='prediction')
+                ax.plot(t,y[0,on:on+40,0].detach().cpu().numpy(),label='data')
+                ax.legend()
+                ax.set_title("dy2")
+                ax.set_xlabel("time (s)")
+                plt.show()
+                plt.close()
+                #assert False
             
-            yhat,state_pred = model(x,y) #state: B x L x SD
-            yhat = yhat * dt_scale
+            #yhat = torch.cat([dy[:,:-1,:] + yhat[:,:-1,:], yhat[:,1:,:]],dim=-1)
             
-            yhat = torch.cat([dy + yhat, yhat],dim=-1)
-            y = torch.cat([dy[:,1:,:],dy2],dim=-1)#torch.cat([y[:,1:,:],dy[:,1:,:],dy2],dim=-1)
+            #y = torch.cat([dy[:,1:,:],dy2[:,1:,:]],dim=-1)#torch.cat([y[:,1:,:],dy[:,1:,:],dy2],dim=-1)
+            """
+            ax = plt.gca()
+            ax.plot(yhat[0,:40,0].detach().cpu().numpy())
+            ax.plot(y[0,:40,0].detach().cpu().numpy())
+            plt.show()
+            plt.close()
 
+            ax = plt.gca()
+            ax.plot(yhat[0,:40,1].detach().cpu().numpy())
+            ax.plot(y[0,:40,1].detach().cpu().numpy())
+            plt.show()
+            plt.close()
+
+            assert False
+            """
+            #print(y[0,:3,:])
+            #assert False
             ### trend filtering penalty
             #diff = torch.diff(state_pred,dim=1)
             #penalty = torch.abs(diff).sum(dim=-1).mean()
@@ -43,7 +121,7 @@ def train(model,optimizer,loss_fn,loaders,filter=None,scheduler=None,nEpochs=100
             alpha = 1/(state_pred.shape[-1] * state_pred.shape[-2])
             norm2 = torch.linalg.vector_norm(state_pred,ord=0.5,dim=-1)
             norm1 = torch.linalg.vector_norm(norm2,ord=1,dim=-1)
-            penalty = alpha*norm1.mean()
+            #penalty = alpha*norm1.mean()
             """
             ### covariance penalty
             cov = state_pred.transpose(-1,-2) @ state_pred /L 
@@ -59,13 +137,16 @@ def train(model,optimizer,loss_fn,loaders,filter=None,scheduler=None,nEpochs=100
             ##################################
 
             
-            mse = loss_fn(y,yhat[:,:-1,:]) #+ loss_fn(dy[:,2:,:],dyhat)# last channel, which is audio. only want to predict this!
-            l = mse #+ penalty
+            mse = loss_fn(y,yhat[:,:L,:]) #+ loss_fn(dy[:,2:,:],dyhat)# last channel, which is audio. only want to predict this!
+            alpha = max(0,min(1,(idx-10*len(loaders['train']))/5000))
+            l = mse + alpha*penalty
+            #print(l)
             l.backward()
             optimizer.step()
             train_losses.append((mse.item(),penalty.item()))
             writer.add_scalar('Loss/train',mse.item(),idx)
             writer.add_scalar('Penalty/train',penalty.item(),idx)
+            model._clip_weights()
             #writer.add_scalar('State norm/train',norm_penalty.item(),idx)
 
         if epoch % val_freq == 0:
@@ -76,20 +157,24 @@ def train(model,optimizer,loss_fn,loaders,filter=None,scheduler=None,nEpochs=100
             for idx,batch in enumerate(loaders['val'],start=epoch*len(loaders['train'])):
                 with torch.no_grad():
                     x,y = batch
-                    x = x.to('cuda')
-                    y = y.to('cuda')
+                    x = x.to('cuda').to(torch.float64)
+                    y = y.to('cuda').to(torch.float64)
                     
-                    dy = y - x
-                    dy2 = torch.diff(dy,dim=1)
-
+                    dy = deriv_approx_dy(y)
+                    # dy_2dt, dy_3dt, ...., dy_(L-2)dt
+                    dy2 = deriv_approx_d2y(y)
+                    # d2y_4dt, d2y_5dt, ..., d2y_(L-4)dt            
                     
-                    yhat,state_pred = model(x,y) #state: B x L x SD
-                    yhat = yhat * dt_scale
+                    yhat,state_pred,penalty = model(x,dt,idx) #state: B x L x SD
+                    yhat = yhat * (model.tau*dt)**2
+                    #yhat = torch.cat([yhat[:,:-1,:] + dy[:,:-1,:],yhat[:,1:,:]],dim=-1) # one-step prediction too
+                    y = dy2 #torch.cat([dy[:,1:,:],dy2[:,1:,:]],dim=-1)
+                    #yhat = torch.cat([dy[:,:-1,:] + yhat[:,:-1,:]/dt_scale * dt, yhat[:,1:,:]],dim=-1)
                     
-                    yhat = torch.cat([dy + yhat, yhat],dim=-1)
-                    y = torch.cat([dy[:,1:,:],dy2],dim=-1)
-                    
-                    l = loss_fn(y,yhat[:,:-1,:])
+                    #y = torch.cat([dy[:,1:,:],dy2[:,1:,:]],dim=-1)
+                    #y = dy2
+                    L = y.shape[1]
+                    l = loss_fn(y,yhat[:,:L,:])
     
                     vl += l.item()
 
@@ -102,7 +187,7 @@ def train(model,optimizer,loss_fn,loaders,filter=None,scheduler=None,nEpochs=100
                     alpha = 1/(state_pred.shape[-1] * state_pred.shape[-2])
                     norm2 = torch.linalg.vector_norm(state_pred,ord=0.5,dim=-1)
                     norm1 = torch.linalg.vector_norm(norm2,ord=1,dim=-1)
-                    penalty = alpha * norm1.mean()
+                    #penalty = alpha * norm1.mean()
                     #vp += alpha*penalty.item()
                     """
                     #### cov penalty
