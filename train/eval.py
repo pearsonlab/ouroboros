bb83cf3e682b72bc7b9f577cb0f3b517016dbe05
mdmarti <miles.martinez@duke.edu>
2025-02-10 13:41:46 -0500

    updating some evaluation on integration -- need to test

--- train/eval.py
+++ train/eval.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 from tqdm import tqdm
-from utils import deriv_approx_d2y,deriv_approx_dy
+from utils import deriv_approx_d2y,deriv_approx_dy,remove_rm,integrate_d2y
 import matplotlib.pyplot as plt
 
 
@@ -66,25 +66,31 @@ def eval_model_error(dls,model,dt):
     train_mu,train_sd = np.nanmean(train_errors),np.nanstd(train_errors)
     return (train_mu,test_mu),(train_sd,test_sd)
 
-def integrate_rm(data,rm_length=5):
 
-    """
-    fill this in with code from your notebook
-    """
-    pass 
 
 def eval_model_integration(dls,model,dt,n_segs=100):
 
-    for seg in n_segs:
+    max_segs = min(min(len(dls['train'],len(dls['val']))),n_segs)
 
-        train_b = next(iter(dls['train']))
-        test_b = next(iter(dls['val']))
+    train_choices = np.random.choice(len(dls['train']),max_segs,replace=False)
+    test_choices = np.random.choice(len(dls['val']),max_segs,replace=False)
+    for train_ind,test_ind in zip(train_choices,test_choices):
+
+        train_b = dls['train'].data[train_ind]
+        test_b = dls['val'].data[test_ind]
 
         train_x,train_y = train_b
         test_x,test_y = test_b 
+        train_x,test_x = train_x.view(1,len(train_x),1),test_x.view(1,len(test_x),1)
+        _,L,_ = train_x.shape
 
         test_dy2,train_dy2 = deriv_approx_d2y(test_x)/(dt**2), deriv_approx_d2y(train_x)/(dt**2)
+        test_dy,train_dy = deriv_approx_dy(test_x)/(dt), deriv_approx_dy(train_x)/(dt)
+        z0_test,z0_train = np.hstack([test_x[0,0,0],test_dy[0,0,0]]),np.hstack([train_x[0,0,0],train_dy[0,0,0]])
+        t = np.arange(0,len(test_x)*dt + dt/2,dt)[:L]
 
+        test_yint = remove_rm(integrate_d2y(test_dy2,t_samples=t,init_cond=z0_test))
+        train_yint = remove_rm(integrate_d2y(train_dy2,t_samples=t,init_cond=z0_train))
     
 
 
