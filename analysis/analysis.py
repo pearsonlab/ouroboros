fd5f58c08d08dae47e96c40dbb9a365adb599db8
mdmarti <miles.martinez@duke.edu>
2025-05-19 14:37:50 -0400

    adding lots of functionality for model params for budgie,marmo

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -5,6 +5,7 @@ from model.model_utils import smooth
 from utils import deriv_approx_d2y,deriv_approx_dy
 from data.real_data import get_segmented_audio
 import vocalpy as voc
+import os
 
 C = 343
 L = 0.035
@@ -151,6 +152,78 @@ def pad_with_nans(x,target_length,axis=0):
         return np.concatenate([x,padding],axis=axis)
     else:
         return x
+
+
+def get_model_fncs(model,audios,dt,smoothing=True,smooth_len=0.005):
+
+    orig_smooth_len = model.smooth_len
+    model.smooth_len = smooth_len
+
+    weights,omegas,gammas,kernels = [],[],[],[]
+    for x in audios:
+
+        xdot= torch.from_numpy(deriv_approx_dy(x)).to(model.device).to(torch.float32)
+
+        #print(xdot[:10])
+        #print(xdot[:10]/dt)
+        # dx: dx_4dt,dx_5dt,dx_6dt,..., dx_(l-4)dt
+        xddot = torch.from_numpy(deriv_approx_d2y(x)).to(model.device).to(torch.float32)/dt**2
+        sample_ax = np.arange(0,xddot.shape[1]*dt + dt/2,dt)[:xddot.shape[1]]
+        #xddot_hat = make_interp_spline(sample_ax,xddot.detach().cpu().numpy().squeeze())
+
+        x = torch.from_numpy(x).to(model.device).to(torch.float32)
+     
+
+        #### get functions from model (assumes we only get one x at a time
+        omega,gamma,weighted_kernel,weight,states = model.get_funcs(x,xdot,dt,scaled=True,smoothing=smoothing)
+        
+        omega,gamma,weighted_kernel= omega.detach().cpu().numpy().squeeze(),gamma.detach().cpu().numpy().squeeze(),\
+                        weighted_kernel.detach().cpu().numpy().squeeze()
+        weight = weight.detach().cpu().numpy().reshape([weight.shape[0],weight.shape[1],-1]).squeeze()
+        weights.append(weight)
+        kernels.append(weighted_kernel)
+        omegas.append(omega)
+        gammas.append(gamma)
+
+    model.smooth_len = orig_smooth_len
+
+    return omegas,gammas,kernels,weights
+
+def get_marmo_fncs(model,audio_location,seg_location,marmo_name,voctype='',seed=None):
+
+    audio_location = os.path.join(audio_location,marmo_name,'wavfiles','synchro_cleaned')
+    seg_location = os.path.join(seg_location,marmo_name,'wavfiles')
+
+    audios,sr = get_segmented_audio(audio_location,seg_location,audio_subdir='',\
+                            seg_subdir='',envelope=False,context_len=0.15,\
+                            audio_type=f'{voctype}_cleaned.wav',seg_type=f'{voctype}.txt',\
+                                max_pairs=3000,seed=seed,full_vocs=True,extend=False)
+    
+    audios = [a[0] for a in audios]
+    omegas,gammas,kernels,weights = get_model_fncs(model,audios,1/sr)
+
+    return omegas,gammas,kernels,weights
+
+def get_budgie_fncs(model,audio_location,seg_location,seed=None,cut_percentile=75):
+
+    audios,sr = get_segmented_audio(audio_location,seg_location,audio_subdir='',\
+                                seg_subdir='',envelope=False,context_len=0.15,\
+                                audio_type='_cleaned.wav',seg_type='izationTimestamp.txt',\
+                                    max_pairs=3000,seed=seed,full_vocs=True,extend=False)
+
+    audios = audios[0]
+    audio_lens = np.array(list(map(lambda y: y.shape[1],audios)))
+    audio_lens = np.hstack(audio_lens)
+
+    ### warbles are relatively long -- so take the longest, idk, 25% of vocalizations
+    min_len = np.percentile(audio_lens,cut_percentile)
+    aud_inds = np.argwhere(audio_lens > min_len)
+    audios = [audios[o] for o in aud_inds]
+
+    omegas,gammas,kernels,weights = get_model_fncs(model,audios,1/sr)
+
+    return omegas,gammas,kernels,weights
+    
 
 def assess_variability(model,audio_location,seg_location,\
                        audio_filetype='.wav',seg_filetype='.txt',max_samples=5000,\
