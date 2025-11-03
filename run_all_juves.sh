150114b2f75da1fb9457aef6fa333b11e5819364
mdmarti <miles.martinez@duke.edu>
2025-11-03 16:50:56 -0500

    paths update

--- run_all_juves.sh
+++ run_all_juves.sh
@@ -4,7 +4,7 @@ CAGBirds=("blk521" "org512" "org545" "pur567")
 CaMKIIBirds=("grn573" "blk603")
 
 for bird in ${CAGBirds[@]}; do
-    python /hdd/miles/ouroboros/hyperparameter_cv.py --audio_path="/home/mmartinez/servers/pearson/birds/mooney/CAGBirds/$bird/data"\
+    python /home/mmartinez/github/ouroboros/hyperparameter_cv.py --audio_path="/home/mmartinez/servers/pearson/birds/mooney/CAGBirds/$bird/data"\
     --audio_subdir="[0-9]*[0-9][0-9]/synchro_cleaned_v2"  --seg_subdir="[0-9]*[0-9][0-9]/denoised_segments" \
     --audio_filetype="_cleaned.wav" --seg_filetype="nrd.txt"\
     --model_path="/home/miles/isilon/All_Staff/miles/models/ouroboros/juveniles_11_3_25_$bird" --nEpochs=40 --seed=92\
@@ -12,7 +12,7 @@ for bird in ${CAGBirds[@]}; do
 done
 
 for bird in ${CaMKIIBirds[@]}; do
-    python /hdd/miles/ouroboros/hyperparameter_cv.py --audio_path="/home/mmartinez/servers/pearson/birds/mooney/CaMKIIBirds/$bird/data"\
+    python /home/mmartinez/github/ouroboros/hyperparameter_cv.py --audio_path="/home/mmartinez/servers/pearson/birds/mooney/CaMKIIBirds/$bird/data"\
     --audio_subdir="[0-9]*[0-9][0-9]/synchro_cleaned_v2"  --seg_subdir="[0-9]*[0-9][0-9]/denoised_segments" \
     --audio_filetype="_cleaned.wav" --seg_filetype="nrd.txt"\
     --model_path="/home/miles/isilon/All_Staff/miles/models/ouroboros/juveniles_11_3_25_$bird" --nEpochs=40 --seed=92\
