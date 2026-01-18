5991169060f42044523cd4ebfe2f9801065fb9b9
mdmarti <miles.martinez@duke.edu>
2026-01-18 09:35:02 -0500

    more verbose for fixing my issues

--- data/real_data.py
+++ data/real_data.py
@@ -189,7 +189,7 @@ def get_segmented_audio(audiopath,segpath,audio_subdir='',seg_subdir='',\
     
     audio_dirs,seg_dirs  = filter_by_tags(audio_dirs,seg_dirs,audio_tags,seg_tags)
     assert len(audio_dirs) >0, \
-        print(f"something went wrong with filtering! i recieved {audiopath},{segpath} as paths,{audio_subdir},{seg_subdir} as subdirs")
+        print(f"something went wrong with filtering! i recieved {audiopath},{segpath} as paths,{audio_subdir},{seg_subdir} as subdirs, found {audio_tags},{seg_tags} as tags")
     #print(audio_dirs,seg_dirs)
     #days = glob.glob(os.path.join(data_dir,'[0-9]*[0-9]'))
     #days = [d.split('/')[-1] for d in wav_dirs]
