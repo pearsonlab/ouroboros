ee6dcada167328d34ec7ccf40719c427c1285ce0
mdmarti <miles.martinez@duke.edu>
2026-05-07 14:37:27 -0400

    fully clean branch

--- run_all_musci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-musciBirds=("blk445" "blk446" "blk447" "org666" "red453")
-
-for bird in ${musciBirds[@]}; do 
-    python /hdd/miles/ouroboros/run_muscimol_experiments.py \
-    --audio_path="/home/miles/isilon/All_Staff/birds/mooney/muscimol/Microdialysis/Muscimol/$bird/" \
-    --model_path="/home/miles/isilon/All_Staff/miles/models/ouroboros/musci_11_21_25_updatedderiv_$bird/"
-done
